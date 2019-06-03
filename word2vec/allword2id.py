9202b000db20b44a71365d23c4fc6ae197dedccc
hm <33774787+nkmeng@users.noreply.github.com>
2019-06-03 13:31:01 +0800

    Update allword2id.py

--- word2vec/allword2id.py
+++ word2vec/allword2id.py
@@ -7,18 +7,31 @@
 将all_word_list转变成allword2id
 """
 
-wordList=[]
-with open("all_word_list.txt",encoding="UTF-8",mode="r+") as f:
-    for line in f:
-        line = line.replace("\n", "")
-        wordList.append(line)
-f.close()
+word_list_f="dataset_all_word.txt"
+word2id_f="dataset_allword2id.txt"
 
-with open("allword2id.txt",encoding="UTF-8",mode="w+") as f:
-    i=0
-    for word in wordList:
-        f.write(str(i)+" ")
-        f.write(word+"\n")
-        i+=1
-f.close()
+
+def generate_word2id(word_list_f,word2id_f):
+    wordList=[]
+    with open(word_list_f,encoding="UTF-8",mode="r+") as f:
+        for line in f:
+            line = line.replace("\n", "")
+            wordList.append(line)
+    f.close()
+
+    with open(word2id_f,encoding="UTF-8",mode="w+") as f:
+        i=0
+        for word in wordList:
+            f.write(str(i)+" ")
+            f.write(word+"\n")
+            i+=1
+    f.close()
+
+def main():
+    generate_word2id(word_list_f, word2id_f)
+
+if __name__ == '__main__':
+    main()
+
+
 
